b779d3010f2ecbcca6bfce866a6483ea4382caf5
Nathan vaartjes <nathan.jan.vaartjes@gmail.com>
2022-12-08 12:16:28 +0100

    finished my version, just need to find a way to visualize game

--- assignment3.py
+++ assignment3.py
@@ -8,95 +8,19 @@ Created on Sat Dec  3 13:46:14 2022
 
 import numpy as np
 import copy
-from functools import cache
+from collections import defaultdict
 
 
-class TicTacToe():
+class TicTacToe:
 
     def __init__(self):
         # to not get confused, bind X with 1 and O with 2
         self.X = 1
         self.O = 2
 
-    # @cache
-    def get_Q_values(self, gameboard_copy):
-        
-        ### make X and O move and get value recursively
-        
-        #get empty cells
-        empty_cells = np.where(gameboard_copy == 0)
-    
-        values_O = []
-        # for every possible O move,
-        for X, Y in zip(empty_cells[0], empty_cells[1]):
-            
-            gameboard_copy[X, Y] = self.O
-            
-            #check if draw
-            if not np.any(gameboard_copy == 0):
-                return 0
-
-            # check if terminal state
-            X_win, Y_win = self.check_if_win(gameboard_copy)
-            if X_win:
-                return 1
-            elif Y_win:
-                return -1
-
-            values_X = []
-
-            # for every X move
-            empty_cells = np.where(gameboard_copy == 0)
-            for x, y in zip(empty_cells[0], empty_cells[1]):
-                gameboard_copy[x, y] = self.X
-
-                # check if terminal state
-                X_win, Y_win = self.check_if_win(gameboard_copy)
-                if X_win:
-                    return 1
-                elif Y_win:
-                    return -1
-
-                value = self.get_Q_values(gameboard_copy)
-                values_X.append(value)
-                gameboard_copy[x, y] = 0
-
-            best_X = max(values_X)
-
-            values_O.append(best_X)
-            # reset board after trying move
-            gameboard_copy[X, Y] = 0
-
-        return np.mean(values_O)
-
-    def construct_gameboard(self):
         # 0 is empty, 1 is X, 2 is O
         self.gameboard = np.zeros((3, 3))
         self.gameboard[1, 1] = self.O
-
-    def make_random_move_O(self):
-        # get empty cells
-        empty_cells = np.where(self.gameboard == 0)
-        # choose 1 empty cell
-        choice = np.random.randint(0, len(empty_cells[0]))
-        # get coordinates of schosen cell and put O
-        X, Y = empty_cells[0][choice],  empty_cells[1][choice]
-        self.gameboard[X, Y] = self.O
-
-    def player_move_X(self):
-        # implement player startegy here
-        # for now, random
-        empty_cells = np.where(self.gameboard == 0)
-        values = []
-        gameboard_copy = copy.deepcopy(self.gameboard)
-        for X, Y in zip(empty_cells[0], empty_cells[1]):
-            gameboard_copy[X, Y] = self.X
-            values.append(self.get_Q_values(gameboard_copy))
-            gameboard_copy[X, Y] = 0
-        move = np.argmax(values)
-        moveX, moveY = empty_cells[0][move], empty_cells[1][move]
-
-        self.gameboard[moveX, moveY] = self.X
 
     def check_if_win(self, gameboard) -> (bool, bool):  # returns X_win, O_win
         # check rows
@@ -120,6 +44,115 @@ class TicTacToe():
 
         return False, False
 
+    def make_random_move_O(self):
+        # get empty cells
+        empty_cells = np.where(self.gameboard == 0)
+        # choose 1 empty cell
+        choice = np.random.randint(0, len(empty_cells[0]))
+        # get coordinates of schosen cell and put O
+        X, Y = empty_cells[0][choice],  empty_cells[1][choice]
+        self.gameboard[X, Y] = self.O
+
+    def player_move_X(self, budget):
+
+        values = []
+        empty_cells = np.where(self.gameboard == 0)
+        gameboard_copy = copy.deepcopy(self.gameboard)
+        # for every possible move of X
+        for X, Y in zip(empty_cells[0], empty_cells[1]):
+            # make hypothetical move
+            gameboard_copy[X, Y] = self.X
+            # get its Q value
+            value = self.calculate_Q(gameboard_copy, budget)
+            values.append(value)
+            # reset move
+            gameboard_copy[X, Y] = 0
+        # get best move
+        move = np.argmax(values)
+        # make move
+        moveX, moveY = empty_cells[0][move], empty_cells[1][move]
+
+        # update board in-place
+        self.gameboard[moveX, moveY] = self.X
+
+    def calculate_Q(self, board, budget: int) -> float:
+        """" returns Q-value of board as float """
+
+        Q_values_dict = defaultdict(int)
+
+        # update Q-values for n iterations
+        # TODO: change to while loop, until convergence
+        for i in range(budget):
+            Q_values_dict = self.get_Q_values(board, Q_values_dict)
+
+        return Q_values_dict[str(board)]
+
+    def get_Q_values(self, gameboard_copy, Q_values_dict):
+
+        ### make X and O move and get value recursively
+
+        # get empty cells
+        empty_cells = np.where(gameboard_copy == 0)
+
+        # take random O move,
+        choice = np.random.randint(0, len(empty_cells[0]))
+        # get coordinates of chosen cell and put O
+        gameboard_copy_level2 = copy.deepcopy(gameboard_copy)
+        X, Y = empty_cells[0][choice], empty_cells[1][choice]
+        gameboard_copy_level2[X, Y] = self.O
+
+        # check if draw
+        if not np.any(gameboard_copy_level2 == 0):
+            Q_values_dict[str(gameboard_copy_level2)] = 0
+            return  Q_values_dict
+
+        X_win, Y_win = self.check_if_win(gameboard_copy_level2)
+        if X_win:
+            Q_values_dict[str(gameboard_copy_level2)] = 1
+            return  Q_values_dict
+        elif Y_win:
+            Q_values_dict[str(gameboard_copy_level2)] = -1
+            return Q_values_dict
+
+        # then, make random X move
+        empty_cells = np.where(gameboard_copy_level2 == 0)
+        choice = np.random.randint(0, len(empty_cells[0]))
+        # get coordinates of chosen cell and put O
+        X, Y = empty_cells[0][choice], empty_cells[1][choice]
+        gameboard_copy_level2[X, Y] = self.X
+
+        # check if terminal state (cant be draw, O always ends)
+        X_win, Y_win = self.check_if_win(gameboard_copy_level2)
+        if X_win:
+            Q_values_dict[str(gameboard_copy_level2)] = 1
+            return Q_values_dict
+        elif Y_win:
+
+            Q_values_dict[str(gameboard_copy_level2)] = -1
+            return Q_values_dict
+
+        # if not terminal state, get Q value recursively
+        Q_values_dict = self.get_Q_values(
+            gameboard_copy_level2, Q_values_dict)
+
+        # update scores via child Q-values
+        # score is the max of the children
+        # average over moves of O, maxify over moves of X:
+        X_values = []
+        for Xx, Yx in zip(empty_cells[0], empty_cells[1]):
+            gameboard_copy[Xx, Yx] = self.X
+            O_values = []
+            empty_cells = np.where(gameboard_copy == 0)
+            for Xy, Yy in zip(empty_cells[0], empty_cells[1]):
+                gameboard_copy[Xy, Yy] = self.O
+                O_values.append(Q_values_dict[str(gameboard_copy)])
+                gameboard_copy[Xy, Yy] = 0
+            X_values.append(np.mean(O_values))
+            gameboard_copy[Xx, Yx] = 0
+        Q_values_dict[str(gameboard_copy)] = np.max(X_values)
+
+        return Q_values_dict
+
     def return_gameboard(self) -> np.array:
         gameboard_char = np.empty((3, 3), dtype='object')
         for i, row in enumerate(self.gameboard):
@@ -127,14 +160,12 @@ class TicTacToe():
                 gameboard_char[i, j] = 'X' if cell == 1 else 'O' if cell == 2 else ''
         return gameboard_char
 
-    def run(self):
-
-        self.construct_gameboard()
+    def run(self, budget: int):
 
         for move in range(4):  # 8 total moves, 4 per player
 
             # player X
-            self.player_move_X()
+            self.player_move_X(budget) # MCTS algo
             X_win, O_win = self.check_if_win(self.gameboard)
             if X_win:
                 return self.X, self.return_gameboard()
@@ -142,7 +173,7 @@ class TicTacToe():
                 return self.O,  self.return_gameboard()
 
             # player O
-            self.make_random_move_O()
+            self.make_random_move_O() # random
             X_win, O_win = self.check_if_win(self.gameboard)
             if X_win:
                 return self.X, self.return_gameboard()
@@ -152,7 +183,8 @@ class TicTacToe():
         return 0,  self.return_gameboard()
 
 
-winner, gameboard = TicTacToe().run()
+winner, gameboard = TicTacToe().run(budget = 200)
+
 if winner == 0:
     print('draw')
 elif winner == 1:
